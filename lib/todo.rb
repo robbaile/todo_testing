22a20b3c34cc46a56e1de60c73447d7f83b78c0a
Rob Baile <robbaile12@outlook.com>
2019-10-14 11:05:26 +0100

    got first test to pass

---
+++ lib/todo.rb
@@ -0,0 +1,5 @@
+class Todo
+    def initialize
+        
+    end
+end
--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -2,7 +2,7 @@ require './lib/todo.rb'
 
 describe Todo do
     it 'should initialise a note class' do
-        today = Todo.new
+        todo = Todo.new
         expect(todo).to be_instance_of(Todo)
     end
 end 
