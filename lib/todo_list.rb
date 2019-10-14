d656f7aa2d557f14a361d659e82507b7a3c817a2
Rob Baile <robbaile12@outlook.com>
2019-10-14 11:33:13 +0100

    added test and code for adding todo to list

--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -1,5 +1,10 @@
 class ToDoList
+    attr_reader :todos
     def initialize
-        
+        @todos = []
+    end
+
+    def add(todo)
+        todos << todo
     end
 end
--- spec/todo_list_spec.rb
+++ spec/todo_list_spec.rb
@@ -1,8 +1,19 @@
 require './lib/todo_list.rb'
+require './lib/todo.rb'
 
 describe ToDoList do 
     it 'should initialise a new ToDoList' do 
         todo_list = ToDoList.new
         expect(todo_list).to be_instance_of(ToDoList)
     end
+
+    it 'should add a new todo to the list' do 
+        todo_list = ToDoList.new
+        todo = Todo.new("Book table")
+
+        todo_list.add(todo)
+
+        expect(todo_list.todos.length).to eq 1
+        expect(todo_list.todos[0]).to be_instance_of(Todo)
+    end 
 end 
